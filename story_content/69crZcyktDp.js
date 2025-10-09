d9525bbc1e8741367ed557c29b4d705e9a17ece5
PSoler31 <paul.soler@ifcdis.fr>
2025-10-09 08:59:59 +0200

    role_de _la_communication_dans_l_accompagnement

---
+++ story_content/69crZcyktDp.js
@@ -0,0 +1,1 @@
+window.globalProvideSvgData('svg-69crZcyktDp', "<svg width=\"1819\" height=\"1080\" viewBox=\"0 0 1819 1080\" fill=\"none\" xmlns=\"http://www.w3.org/2000/svg\" id=\"svg-69crZcyktDp\"><g pointer-events=\"visiblePainted\" data-accepts=\"events\"><circle opacity=\"0.06\" cx=\"909.5\" cy=\"540.5\" r=\"909.5\" fill=\"#2B2B29\" /></g></svg>");
