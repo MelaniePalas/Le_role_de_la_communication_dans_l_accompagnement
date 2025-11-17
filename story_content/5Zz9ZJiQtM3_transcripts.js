c54c331bce5c102d3dbd64ac4fa8d766cfbc1275
PSoler31 <paul.soler@ifcdis.fr>
2025-11-17 17:07:26 +0100

    v - 1711

---
+++ story_content/5Zz9ZJiQtM3_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":208,"text":"Prenez le temps nécessaire pour effectuer\r\nle tour de l'entreprise"},{"start":3376,"text":"dès le premier jour de l'alternant\r\npour l'aider à s'intégrer."},{"start":6958,"text":"En même temps,\r\nfaites le tour de chaque service,"},{"start":9834,"text":"montrez-lui"},{"start":10376,"text":"les différentes issues de secours\r\net présentez-lui ses futurs collègues."}]}]};
+    window.globalLoadJsAsset('story_content/5Zz9ZJiQtM3_transcripts.js', JSON.stringify(data));
+})();
